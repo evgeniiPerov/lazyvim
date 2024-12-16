75737dfbb13877bb7041e3939cd416234ac4cb6f
evgenii <perov@1webit.cz>
2024-12-16 21:06:43 +0100

    feat: fix new update

--- lua/plugins/file-browser.lua
+++ lua/plugins/file-browser.lua
@@ -1,13 +1,21 @@
 return {
-  "nvim-telescope/telescope-file-browser.nvim",
-  keys = {
-    {
-      "<leader>sB",
-      ":Telescope file_browser path=%:p:h=%:p:h<cr>",
-      desc = "Browse Files",
-    },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }, -- Ensure plenary is installed
   },
-  config = function()
-    require("telescope").load_extension("file_browser")
-  end,
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" }, -- Ensure telescope.nvim is a dependency
+    keys = {
+      {
+        "<leader>sB",
+        ":Telescope file_browser path=%:p:h=%:p:h<cr>",
+        desc = "Browse Files",
+      },
+    },
+    config = function()
+      require("telescope").setup({}) -- Basic setup for telescope.nvim
+      require("telescope").load_extension("file_browser")
+    end,
+  },
 }
