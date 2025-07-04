8a81435791bd0e5bfe656f9dbe7ec677960e1b9b
Evgenii <evgeniiperov17@gmail.com>
2025-07-04 15:34:38 +0200

    feat fix eslint

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,57 @@
+return {
+  "yetone/avante.nvim",
+  build = function()
+    if vim.fn.has("win32") == 1 then
+      return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    else
+      return "make"
+    end
+  end,
+  event = "VeryLazy",
+  version = false,
+  opts = {
+    provider = "ollama",
+    providers = {
+      ollama = {
+        endpoint = "http://127.0.0.1:11434",
+        model = "gemma3:latest",
+        timeout = 30000,
+        extra_request_body = {
+          temperature = 0.5,
+          max_tokens = 2048,
+        },
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "echasnovski/mini.pick",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "ibhagwan/fzf-lua",
+    "stevearc/dressing.nvim",
+    "folke/snacks.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = { insert_mode = true },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
